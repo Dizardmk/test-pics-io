9025c0ea12d6528226d7994ac145640498bc9ef4
Dizardmk <Dizardmk@gmail.com>
2024-10-07 10:22:11 +0300

    fix: remove transform translate styles from comments-loader

--- src/components/comments/comments-loader.tsx
+++ src/components/comments/comments-loader.tsx
@@ -2,6 +2,6 @@ import { Loader2 } from "lucide-react";
 
 export default function CommentsLoader() {
   return (
-    <Loader2 className="h-16 w-16 animate-spin absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+    <Loader2 className="h-24 w-24 animate-spin absolute top-1/2 left-1/2" />
   );
 }
